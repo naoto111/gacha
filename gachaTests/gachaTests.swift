aabd76f64cf2b41ad717928c15e418e4aefa72ca
Naoto <naoto60042@icloud.com>
2015-04-24 18:59:55 +0900

    Initial Commit

---
+++ gachaTests/gachaTests.swift
@@ -0,0 +1,36 @@
+//
+//  gachaTests.swift
+//  gachaTests
+//
+//  Created by Naoto on 2015/04/24.
+//  Copyright (c) 2015年 Naoto. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class gachaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
